5f5c00f7d2aa5d6c5f63d622065c5984d39680c5
catalina-macovei <cmacovei01@gmail.com>
2022-10-03 18:41:03 +0300

    tracking the active link

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,21 @@
+function trackActiveLink(el){
+    //making the target link active
+    el.classList.add("active");
+    // getting the target element
+    const target = el.getAttribute("data-link");
 
+    //deactivating previous links
+
+    // selecting all links that contain class active
+    const navLinks = document.querySelectorAll("a.nav-link");
+    
+    /*
+    iterating through the links to identify the links that aren't 
+    active by removing the active class
+    */
+    navLinks.forEach(link => {
+        if(target !== link.getAttribute("data-link")){
+            link.classList.remove("active");
+        }
+    })
+}
